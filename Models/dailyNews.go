be4022bacf30cc673991edd22215fc67d3d9bbfc
naitosama <naitosama@qq.com>
2023-06-06 18:15:53 +0800

    新增新的新闻api

--- Models/dailyNews.go
+++ Models/dailyNews.go
@@ -7,3 +7,26 @@ type DailyNews_01 struct {
 	Data    []string `json:"data"`
 	ImgUrl  string   `json:"imgUrl"`
 }
+
+type DailyNews struct {
+	Code     int    `json:"code"`
+	Message  string `json:"msg"`
+	ImageUrl string `json:"imageUrl"`
+}
+
+type DailyNewsFanXing struct {
+	Code    int                  `json:"code"`
+	Message string               `json:"msg,omitempty"`
+	Data    DailyNewsFanXingData `json:"data,omitempty"`
+	Time    int64                `json:"time,omitempty"`
+	Usage   int                  `json:"usage,omitempty"`
+	LogID   string               `json:"log_id,omitempty"`
+}
+
+type DailyNewsFanXingData struct {
+	Date      string   `json:"date"`
+	News      []string `json:"news"`
+	WeiYu     string   `json:"weiyu"`
+	Image     string   `json:"image"`
+	HeadImage string   `json:"head_image,omitempty"`
+}
--- server/dailyNews.go
+++ server/dailyNews.go
@@ -3,18 +3,24 @@ package server
 import (
 	"UsaBot/Models"
 	"UsaBot/common"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"time"
 )
 
 // DailyNews 每日晨报
 func DailyNews(groups []int64) {
+	err := DailyNewsFanXing(groups)
+	if err != nil {
+		common.Logln(2, err)
+		return
+	}
+}
+
+func DailyNews00(groups []int64) {
 	//resp, err := common.RequestTo("https://api.03c3.cn/zb/api.php", "GET", "", nil)
 	//if err != nil {
 	//	common.Logln(2, err)
@@ -39,48 +45,49 @@ func DailyNews(groups []int64) {
 		common.Logln(2, err)
 		return
 	}
-	respStruct := make(map[string]string, 0)
+	//respStruct := make(map[string]string, 0)
+	respStruct := Models.DailyNews{}
 	err = json.Unmarshal(data, &respStruct)
-	msg, ok := respStruct["msg"]
-	if !ok {
-		common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php未有响应")
-		return
-	}
-	if msg != "Success" {
+	//msg, ok := respStruct["msg"]
+	//if !ok {
+	//	common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php未有响应")
+	//	return
+	//}
+	//if msg != "Success" {
+	//	common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php失败")
+	//	return
+	//}
+	//imageUrl, ok := respStruct["imageUrl"]
+	//if !ok {
+	//	common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php图片网址失败")
+	//	return
+	//}
+	//datatime, ok := respStruct["datatime"]
+	//if !ok {
+	//	common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php 数据更新时间失败")
+	//	return
+	//}
+	if respStruct.Code != 200 {
 		common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php失败")
 		return
 	}
-	imageUrl, ok := respStruct["imageUrl"]
-	if !ok {
-		common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php图片网址失败")
-		return
-	}
-	datatime, ok := respStruct["datatime"]
-	if !ok {
-		common.Logln(2, "获取接口https://api.03c3.cn/zb/api.php 数据更新时间失败")
-		return
-	}
-	picPath := "./pic/dailyNews-" + datatime + ".png"
+	imageUrl := respStruct.ImageUrl
+
+	now := time.Now()
+	dataTime := now.Format("2006-01-02")
+	picPath := "./pic/dailyNews-" + dataTime + ".png"
 	err = common.DownloadPic(picPath, imageUrl)
 	if err != nil {
 		common.Logln(2, err)
 		return
 	}
-	picFile, err := os.Open(picPath)
+
+	picData, err := common.PicBase64(picPath)
 	if err != nil {
 		common.Logln(2, err)
 		return
 	}
-
-	picData, err := io.ReadAll(picFile)
-	if err != nil {
-		common.Logln(2, err)
-		return
-	}
-
-	picBase64 := base64.StdEncoding.EncodeToString(picData)
-
-	content := "早上好，打工人，看看最近都发生什么事儿了\n[CQ:image,file=base64://" + picBase64 + "]"
+	content := fmt.Sprintf("早上好打工人，今天是%d月%d号，看看最近都发生了什么\n%s", now.Month(), now.Day(), picData)
 
 	for _, v := range groups {
 		message := Models.SendGroupMessage{
@@ -138,4 +145,50 @@ func DailyNews01(groupIDs []int64) {
 		}
 
 	}
+}
+
+func DailyNewsFanXing(groupIDs []int64) error {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", "https://api.emoao.com/api/60s?type=json", nil)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	respBody := Models.DailyNewsFanXing{}
+	respBodyData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(respBodyData, &respBody)
+	if err != nil {
+		return err
+	}
+	if respBody.Code != 200 {
+		return errors.New("获取接口数据失败")
+	}
+	now := time.Now()
+	dataTime := now.Format("2006-01-02")
+	picPath := "./pic/dailyNews-" + dataTime + ".png"
+	err = common.DownloadPic(picPath, respBody.Data.Image)
+	if err != nil {
+		return err
+	}
+	weekday := []string{"周日", "周一", "周二", "周三", "周四", "周五", "周六"}
+	picBase, err := common.PicBase64(picPath)
+	if err != nil {
+		return err
+	}
+	for _, v := range groupIDs {
+		content := fmt.Sprintf("早上好打工人，今天是%d月%d日，%s\n为您准备了以下新闻：\n%s", now.Month(), now.Day(), weekday[now.Weekday()], picBase)
+		_, err = common.GroupChatSender(v, content)
+		if err != nil {
+			common.ErrorResponse(true, v, errors.New("每日新闻发送失败"))
+			return err
+		}
+	}
+	return nil
 }
